cf20b1dc3edb4a6455561d61c2d8257b77d447d6
Mirmahyar <m.mirsamadi@gmail.com>
2021-03-02 19:40:09 +0330

    initialization

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuIcon from '@material-ui/icons/Menu';
+
+function Header() {
+    return (
+        <div className = "header-wrapper">
+            
+        </div>
+    )
+}
+
+export default Header
