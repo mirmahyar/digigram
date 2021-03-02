6e71d2e3987f44d4df1128d06880682673c6ec54
Mirmahyar <m.mirsamadi@gmail.com>
2021-03-02 21:20:29 +0330

    Input component initialized

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,12 +4,26 @@ function Input() {
 
     const [message,SetMessage] = useState("");
 
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        alert('You submitted');
+    }
+
     return (
         <div>
+            <form onSubmit={handleSubmit}>
             <input
             type="text" id="input" name="input"
-            value={SetMessage}
-            placeholder="Write a message"/>
+            value={message}
+            placeholder="Write a message"
+            onChange= {(e)=>{
+                SetMessage(e.target.value)
+            }}
+            />
+            <input type="submit" value="Send"/>
+            </form>
+            <p>{message}</p>
+            
 
             
         </div>
